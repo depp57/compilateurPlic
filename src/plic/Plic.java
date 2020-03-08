cc126f17aac28af4dd3a8a060b398e08c8d58fb4
Sacha <deppdu5758@gmail.com>
2020-03-08 18:42:55 +0100

    handle erreur fichier non trouvé

--- src/plic/Plic.java
+++ src/plic/Plic.java
@@ -4,13 +4,15 @@ import plic.analyse.AnalyseurSyntaxique;
 import plic.exception.ErreurSyntaxique;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Plic {
 
     public Plic(String fileName) {
-        AnalyseurSyntaxique as = new AnalyseurSyntaxique(new File(fileName));
         try {
-            as.analyse();
+            File file = new File(fileName);
+            if (!file.exists()) throw new ErreurSyntaxique(fileName + " n'a pas été trouvé");
+            new AnalyseurSyntaxique(file).analyse();
         }
         catch (ErreurSyntaxique e) {
             System.out.println(e.getMessage());
